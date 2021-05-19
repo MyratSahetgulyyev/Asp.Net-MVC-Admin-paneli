0f2cbb5667537d12b3b1938b34f64a5795ee0f82
Myrat Sahetgulyyev <durluyaz@gmail.com>
2021-05-19 19:26:18 +0300

    Admin Paneli
    Asp.Net MVC, Bootstrap kullanararak basit kullanışlı temel kodlar ile admin paneli. (post, get, remove, add, update)

---
+++ Proje Admin Panel/01/Controllers/MusteriController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _01.Models.Entity;
+using PagedList;
+using PagedList.Mvc;
+
+
+namespace _01.Controllers
+{
+    public class MusteriController : Controller
+    {
+        MvcDvStokEntities db = new MvcDvStokEntities();
+        // GET: Musteri
+        public ActionResult Index(int sayfa=1)
+        {
+            var degerler = db.TBLMUSTERI.ToList().ToPagedList(sayfa, 4);
+            return View(degerler);
+        }
+
+
+        [HttpPost]
+        public ActionResult YeniMusteri(TBLMUSTERI p1)
+        {
+            db.TBLMUSTERI.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult YeniMusteri()
+        {
+            return View();
+        }
+
+        //sil
+         
+        public ActionResult SIL (int id)
+        {
+            var musteri = db.TBLMUSTERI.Find(id);
+            db.TBLMUSTERI.Remove(musteri);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        //veri taşı
+        public ActionResult MusteriGetir(int id)
+        {
+            var mstr = db.TBLMUSTERI.Find(id);
+            return View(mstr);
+        }
+
+        //Güncelle
+        public ActionResult Guncelle(TBLMUSTERI p1)
+        {
+            var mst = db.TBLMUSTERI.Find(p1.MUSTERIID);
+            mst.MUSTERIAD = p1.MUSTERIAD;
+            mst.MUSTERISOYAD = p1.MUSTERISOYAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Proje Admin Panel/01/Controllers/UrunController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _01.Models.Entity;
+
+namespace _01.Controllers
+{
+   
+    public class UrunController : Controller
+    {
+        MvcDvStokEntities db = new MvcDvStokEntities();
+        // GET: Urun
+        public ActionResult Index (string p)
+        {
+            var degerler = from d in db.TBLURUNLER select d;
+            if(!string.IsNullOrEmpty(p))
+            {
+                degerler = degerler.Where(m => m.URUNAD.Contains(p));
+            }
+            return View(degerler.ToList());
+            //var degerler = db.TBLURUNLER.ToList();
+            //return View(degerler);
+        }
+         
+
+
+        //ekleme
+        [HttpPost]
+        public ActionResult YeniUrun (TBLURUNLER p1)
+        {
+            var ktg = db.TBLKATEGORİLER.Where(m => m.KATEGORIID == p1.URUNKATEGORİ).FirstOrDefault();
+            p1.TBLKATEGORİLER = ktg;
+            db.TBLURUNLER.Add(p1);
+            db.SaveChanges();
+            return Redirect("Index");
+        }
+
+
+        [HttpGet]
+        public ActionResult YeniUrun()
+        {
+            List<SelectListItem> degerler = (from i in db.TBLKATEGORİLER.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KATEGORIAD,
+                                                 Value = i.KATEGORIID.ToString()
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+            return View();
+        }
+
+        //sil
+        public ActionResult SIL (int id)
+        {
+            var urunler = db.TBLURUNLER.Find(id);
+            db.TBLURUNLER.Remove(urunler);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        //veri taşı
+        public ActionResult UrunGetir(int id)
+        {
+            var urn = db.TBLURUNLER.Find(id);
+            return View(urn);
+        }
+
+        public ActionResult Guncelle(TBLURUNLER p1)
+        {
+            var urn = db.TBLURUNLER.Find(p1.URUNID);
+            
+            urn.URUNAD = p1.URUNAD;
+            urn.MARKA = p1.MARKA;
+
+            urn.URUNKATEGORİ = p1.URUNKATEGORİ;
+
+            urn.FIYAT = p1.FIYAT;
+            urn.STOK = p1.STOK;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //yardım
+        public ActionResult Yardim()
+        {
+            return View();
+        }
+    }
+}
